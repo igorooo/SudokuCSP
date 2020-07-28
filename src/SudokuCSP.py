2443524a0e0c2487d6b092d28df21bcb19a26a1a
Igor Czeczot <igorczeczot@windowslive.com>
2020-07-28 21:31:39 +0200

    done

--- src/SudokuCSP.py
+++ src/SudokuCSP.py
@@ -1,7 +1,8 @@
 import csv
-from random import shuffle
+from random import shuffle, randint, choice
 import numpy as np
 from copy import deepcopy
+import matplotlib.pyplot as plt
 
 
 def parser(filePath):
@@ -32,24 +33,41 @@ class Domain:
                 else:
                     self.domains[(j, i)] = [1,2,3,4,5,6,7,8,9]
 
-    def updateDomain(self, jj, ii, value):
+    def updateDomain(self, jj, ii, value, board):
         f = lambda x: int(x / 3) * 3
+        outOfDomainElems = False
+        exclusions = 0
 
         #square update
         for j in range(f(jj), f(jj)+3):
             for i in range(f(ii), f(ii)+3):
                 if j != jj and i != ii and value in self.domains[(j, i)]:
                     self.domains[(j, i)].remove(value)
+                    exclusions += 1
+
+                    # if no more available values in domain and still no value on board
+                    if len(self.domains[(j, i)]) ==  board[j, i] == 0:
+                        outOfDomainElems = True
 
         #horizontal update
         for i in range(9):
             if i != ii and value in self.domains[(jj, i)]:
                 self.domains[(jj, i)].remove(value)
+                exclusions += 1
+
+                if len(self.domains[(jj, i)]) == board[jj, i] == 0:
+                    outOfDomainElems = True
 
         #vertical update
         for j in range(9):
             if j != jj and value in self.domains[(j, ii)]:
                 self.domains[(j, ii)].remove(value)
+                exclusions += 1
+
+                if len(self.domains[(j, ii)]) == board[j, ii] == 0:
+                    outOfDomainElems = True
+
+        return outOfDomainElems, exclusions
 
     def pickSmallestDomain(self, board):
         bestL = 10
@@ -68,6 +86,14 @@ class Domain:
                     return j, i
         raise AssertionError("board filled!")
 
+    def pickRandom(self, board):
+        options = []
+        for j in range(9):
+            for i in range(9):
+                if board[j, i] == 0:
+                    options.append((j, i))
+        return choice(options)
+
 
 
     def getDomain(self, j, i):
@@ -75,6 +101,13 @@ class Domain:
 
 
 class SudokuCSP:
+
+    SMALLEST_DOMAIN = 'Smallest Domain'
+    RANDOM = 'Random'
+    IN_ORDER = 'In Order'
+    LESS_CONSTR = 'Less constrainting'
+
+    TRESHOLD = 1000000
 
     def __init__(self, puzzlePlain):
         board = SudokuCSP.parsePuzzle(puzzlePlain)
@@ -88,29 +121,53 @@ class SudokuCSP:
         self.constraints = [row_constraint, column_constraint, square_constraint]
 
 
-    def backtracingSearchStart(self):
+    def backtracingSearchStart(self, forwardChecking=False, pickVarMethod=SMALLEST_DOMAIN, pickValMethod=RANDOM):
         board = np.zeros((9,9))
         domains = deepcopy(self.domains)
         solutions = []
         self.globalStepsCounter = 0
 
-        self.backtraceingSearch(board, domains, solutions, stepsNumber=0)
+        self.backtraceingSearch(board, domains, solutions, stepsNumber=0, forwardChecking=forwardChecking,
+                                pickVarMethod=pickVarMethod, pickValMethod=pickValMethod)
         return solutions, self.globalStepsCounter
 
 
-    def backtraceingSearch(self, board, domains, solutions, stepsNumber):
+    def backtraceingSearch(self, board, domains, solutions, stepsNumber, forwardChecking, pickVarMethod, pickValMethod):
+        #print(self.globalStepsCounter)
         self.globalStepsCounter += 1
-        j, i = domains.pickSmallestDomain(board)
-        #j, i = domains.pickFirstAvailable(board)
+        if self.globalStepsCounter > SudokuCSP.TRESHOLD:
+            return
 
-        for elem in self.pickValRandom(domains.getDomain(j, i)):
+        j, i = 0, 0
+        if pickVarMethod == SudokuCSP.RANDOM:
+            j, i = domains.pickRandom(board)
+        elif pickVarMethod == SudokuCSP.IN_ORDER:
+            j, i = domains.pickFirstAvailable(board)
+        else:
+            j, i = domains.pickSmallestDomain(board)
+
+        values = None
+        if pickValMethod == SudokuCSP.RANDOM:
+            values = self.pickValLessConstr(domains.getDomain(j, i), j, i, domains, board)
+        elif pickValMethod == SudokuCSP.IN_ORDER:
+            values = self.pickValLessConstr(domains.getDomain(j, i), j, i, domains, board)
+        else:
+            values = self.pickValLessConstr(domains.getDomain(j, i), j, i, domains, board)
+
+        for elem in values:
             if self.checkConstraints(j, i, elem, board):
                 tempBoard = deepcopy(board)
                 tempBoard[j, i] = elem
                 dom = deepcopy(domains)
-                dom.updateDomain(j, i, elem)
+
+                outOfAvailableDomainElems, _ = dom.updateDomain(j, i, elem, tempBoard)
+                if forwardChecking and outOfAvailableDomainElems:
+                    #then omit current tree
+                    continue
+
                 if stepsNumber < 80:
-                    self.backtraceingSearch(tempBoard, dom, solutions, stepsNumber + 1)
+                    self.backtraceingSearch(tempBoard, dom, solutions, stepsNumber + 1, forwardChecking,
+                                            pickVarMethod, pickValMethod)
                 if stepsNumber >= 80:
                     tempBoard[j, i] = elem
                     solutions.append(tempBoard)
@@ -118,20 +175,28 @@ class SudokuCSP:
 
 
 
-    def pickValRandom(self, domain):
+    def pickValRandom(self, domain, j, i, domains, board):
         tmp = domain[:]
         shuffle(tmp)
         return tmp
 
-    def pickValInOrder(self, domain):
+    def pickValInOrder(self, domain, j, i, domains, board):
         return domain[:]
+
+    def pickValLessConstr(self, domain, j, i, domains, board):
+        arr = []
+        for elem in domain:
+            doms = deepcopy(domains)
+            _, exclusives = doms.updateDomain(j, i, elem, board)
+            arr.append((elem, exclusives))
+        res = [x[0] for x in sorted(arr, key=lambda x : x[1], reverse=True)]
+        return res
 
     def checkConstraints(self, column, row, value, board):
         for constraint in self.constraints:
             if constraint(column, row, value, board) == False:
                 return False
         return True
-
 
     @staticmethod
     def parsePuzzle(puzzlePlain):
@@ -160,18 +225,109 @@ if __name__ == '__main__':
     sudoku_tasks = parser('../data/Sudoku.csv')
     #puzzlePlain = '625..1..8.7..8...68........2....4....4..1.3........1.......7.32.....6.9..84.3....'
 
+    # first number
+    # 1 - SMALLEST DOMAIN VAR
+    # 2 - RANDOM VAR
+    # 3 - IN ORDER VAR
+
+    # second number
+    # 1 - LESS CONSTRAINTING VAL
+    # 2 - RANDOM VAL
+    # 3 - IN ORDER VAL
+
+    # third number
+    # 1 - with forward checking
+    # 0 - without forward checking
+
+    results = {
+        '110' : [],
+        '120' : [],
+        '130' : [],
+        '121' : [],
+        '211' : [],
+        '221' : [],
+        '231' : [],
+        '311' : [],
+        '321' : [],
+        '331' : [],
+    }
+
+    iter = 0
+
     for task in sudoku_tasks:
-        sudoku = SudokuCSP(task['puzzle'])
-        solutions, steps = sudoku.backtracingSearchStart()
         print("ID: %s,  DIFFICULTY: %s" % (task['id'], task['difficulty']))
-        print('Total steps: %d' % (steps))
-        print("Solutions: %d" % (len(solutions)))
-        for num, sol in enumerate(solutions):
-            print("------ Solution number: %d -----" % (num+1))
-            print(sol)
-        if "solution" in task:
-            print("Given solution:")
-            print(SudokuCSP.parsePuzzle(task['solution']))
+        sudoku = SudokuCSP(task['puzzle'])
+
+        results['110'].append(sudoku.backtracingSearchStart(forwardChecking=False,
+                                                         pickVarMethod=SudokuCSP.SMALLEST_DOMAIN,
+                                                         pickValMethod=SudokuCSP.LESS_CONSTR))
+
+        results['120'].append(sudoku.backtracingSearchStart(forwardChecking=False,
+                                                            pickVarMethod=SudokuCSP.SMALLEST_DOMAIN,
+                                                            pickValMethod=SudokuCSP.RANDOM))
+
+        results['130'].append(sudoku.backtracingSearchStart(forwardChecking=False,
+                                                            pickVarMethod=SudokuCSP.SMALLEST_DOMAIN,
+                                                            pickValMethod=SudokuCSP.IN_ORDER))
+
+        results['121'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.SMALLEST_DOMAIN,
+                                                            pickValMethod=SudokuCSP.RANDOM))
+
+        results['211'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.RANDOM,
+                                                            pickValMethod=SudokuCSP.LESS_CONSTR))
+
+        results['221'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.RANDOM,
+                                                            pickValMethod=SudokuCSP.RANDOM))
+
+        results['231'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.RANDOM,
+                                                            pickValMethod=SudokuCSP.IN_ORDER))
+
+        results['311'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.IN_ORDER,
+                                                            pickValMethod=SudokuCSP.LESS_CONSTR))
+
+        results['321'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.IN_ORDER,
+                                                            pickValMethod=SudokuCSP.RANDOM))
+
+        results['331'].append(sudoku.backtracingSearchStart(forwardChecking=True,
+                                                            pickVarMethod=SudokuCSP.IN_ORDER,
+                                                            pickValMethod=SudokuCSP.IN_ORDER))
+
+        iter += 1
+        break
+
+
+    stepsArr = [i for i in range(1, iter + 1)]
+
+
+    for key, result in results.items():
+
+        noOfSteps = []
+        noOfSolutions = []
+
+        for res in result:
+            noOfSolutions.append(len(res[0]))
+            noOfSteps.append(res[1])
+
+
+        plt.figure().suptitle('Wyniki(' + key + ')', fontsize=10)
+        plt.plot(stepsArr, noOfSolutions, 'r+')
+        plt.legend()
+        plt.show()
+        plt.clf()
+        plt.figure().suptitle('Liczb kroków(' + key + ')', fontsize=10)
+        plt.plot(stepsArr, noOfSteps)
+        plt.legend()
+        plt.show()
+
+
+
+
 
 
 
